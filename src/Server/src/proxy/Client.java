5b73d5359556b25c2b55db2458ac59545a1bb98f
Yu-Tzu Lin <linyutsu23@gmail.com>
2016-03-28 02:45:40 +0800

    new proxy server source code file

---
+++ src/Server/src/proxy/Client.java
@@ -0,0 +1,15 @@
+package proxy;
+
+/*		*
+ * 	File:						proxy/Client.java
+ * 
+ * 	Use:						The SIP proxy server. 
+ * 								It will according the SIP method to tranfer the packet to registrar server or redirect server								
+ * 
+ * 	Update Date: 	2016.  3. 28
+ * */
+
+
+public class Client {
+
+}
---
+++ src/Server/src/proxy/Server.java
@@ -0,0 +1,15 @@
+package proxy;
+
+/*		*
+ * 	File:						proxy/Server.java
+ * 
+ * 	Use:						The SIP proxy server. 
+ * 								It will listen to wait User Agent to connect.
+ * 								And pass the packet to proxy/Client.java to do something    
+ * 
+ * 	Update Date: 	2016.  3. 28
+ * */
+
+public class Server {
+
+}
